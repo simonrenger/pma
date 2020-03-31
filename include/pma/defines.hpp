814beb55009131ffda875196dcd7ebe8cadaed8a
Simon Renger <simonrenger@gmail.com>
2020-04-01 00:26:31 +0200

    [*] added maybe unused

--- include/pma/defines.hpp
+++ include/pma/defines.hpp
@@ -5,9 +5,11 @@
 	#define assertm(exp, msg) assert(((void)msg, exp))
 
 	#if __cplusplus == 201703L
+	#define MAYBE_UNUSED [[maybe_unused]]
 	#define NO_DISCARD [[nodiscard]]
 	#else
-	#define NO_DISCARD 
+	#define NO_DISCARD
+	#define MAYBE_UNUSED
 	#endif
 
 #endif
