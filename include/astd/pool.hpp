37a2273720a1079f7141fce30693c7d77b98c64c
Simon Renger <simonrenger@gmail.com>
2020-04-01 00:28:34 +0200

    [*] added pool allocation startegy

--- include/astd/pool.hpp
+++ include/astd/pool.hpp
@@ -2,15 +2,23 @@
 #define PMA_AS_POOL_HPP_INCLUDED
 #include <pma/defines.hpp>
 #include <pma/allocation_strategy.hpp>
+#include <astd/utils/stack_linked_list.hpp>
+
 namespace astd{
+    namespace details {
+        struct free_header {};
+        using node_ptr = utils::stack_linked_list<details::free_header>::node_ptr;
+    };
 class pool : public pma::allocation_strategy{
-       NO_DISCARD  virtual void* do_allocate(const std::size_t count, const std::size_t aligment) final {
-           // impl
-            return nullptr;
-       }
-     virtual void do_deallocate(void* const ptr,const std::size_t count) final {
-       // impl
-     }
+    void* start_ptr{ nullptr };
+    utils::stack_linked_list<details::free_header> free_list{};
+protected:
+    NO_DISCARD  virtual void* do_allocate(const std::size_t size, MAYBE_UNUSED const std::size_t aligment) final;
+    virtual void do_deallocate(void* const ptr, MAYBE_UNUSED const std::size_t count) final;
+    NO_DISCARD virtual bool do_is_equal(const allocation_strategy& that) const noexcept final;
+public:
+    pool(pma::alloc_option option, allocation_strategy* const upstream = nullptr);
+    void release();
 };
 }
 #endif
--- src/astd/pool.cpp
+++ src/astd/pool.cpp
@@ -1,1 +1,34 @@
 #include <astd/pool.hpp>
+#include <pma/utils.hpp>
+
+#include <cstdlib>
+#include <cassert>
+
+astd::pool::pool(pma::alloc_option option, allocation_strategy* const upstream) : allocation_strategy{ option, upstream }{
+	// 8 bytes because the nodes they have pointer size.
+	assertm(option.chunck_size >= 8 , "Chunk size must be greater or equal to 8");
+	assertm(option.buffer_size % option.chunck_size == 0 , "Total Size must be a multiple of Chunk Size");
+	start_ptr = std::malloc(option.buffer_size);
+}
+
+NO_DISCARD void* astd::pool::do_allocate(const std::size_t size, MAYBE_UNUSED const std::size_t) {
+	assertm(size <= option().chunck_size, "size is bigger then chnuck");
+	details::node_ptr free_pos = free_list.pop();
+	assertm(free_pos != nullptr , "The pool allocator is full");
+	return free_pos;
+}
+void astd::pool::do_deallocate(void* const ptr, MAYBE_UNUSED const std::size_t) {
+	free_list.push(reinterpret_cast<details::node_ptr>(ptr));
+}
+
+NO_DISCARD bool astd::pool::do_is_equal(const pma::allocation_strategy& that) const noexcept {
+    return &that == this;
+}
+
+void astd::pool::release() {
+	const auto chuncks{ option().buffer_size / option().chunck_size };
+	for (auto i{ 0 }; i < chuncks; ++i) {
+		std::size_t address = reinterpret_cast<std::size_t>(start_ptr) + i * option().chunck_size;
+		free_list.push(reinterpret_cast<details::node_ptr>(address));
+	}
+}
